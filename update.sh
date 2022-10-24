dbff94c0e6e735b1f6e0fc1a316c5c5686c129e2
Nicola Vetrini <nicola.vetrini@bugseng.com>
2022-10-24 16:47:45 +0200

    test

--- update.sh
+++ update.sh
@@ -32,6 +32,8 @@ if [ "${is_pr}" = 'true' ]; then
     last_dir="${last_dir}/commits/${base_pr_sha}"
 fi
 
+echo "${last_dir}"
+
 last_job_id=
 [ ! -d "${last_dir}" ] || last_job_id=$(basename "$(realpath "${last_dir}")")
 
