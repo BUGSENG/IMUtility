8c6c1c743d3aacffb9b5d143bd3c5f7efe5e2a52
Nicola Vetrini <nicola.vetrini@bugseng.com>
2022-10-24 18:34:06 +0200

    fix

--- update.sh
+++ update.sh
@@ -82,8 +82,8 @@ ln -sfn "../${current_job_id}" "${results_root}/commits/${commit_id}"
 ECLAIR_REPORT_HOST="eclairit.com" # TODO: pass this as a variable
 {
     echo "# ECLAIR analysis summary:"
-    printf "\- Fixed reports: %d\n" "${fixed_reports}"
-    printf "\- New reports: %d\n" "${new_reports}"
+    printf "Fixed reports: %d\n" "${fixed_reports}"
+    printf "New reports: %d\n" "${new_reports}"
     echo "[Browse analysis](https://${ECLAIR_REPORT_HOST}:3787/fs${current_dir}/index.html)"
     echo "*****************************************************"
 } >>"${current_dir}/summary.txt"
