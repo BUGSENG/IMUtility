2d56b923090006ebf3fff4a46f4ba97510ba2570
Abramo Bagnara <abramo.bagnara@bugseng.com>
2022-11-15 12:57:35 +0100

    Fixed typos.

--- ECLAIR/upload.sh
+++ ECLAIR/upload.sh
@@ -2,12 +2,17 @@
 
 set -eu
 
-cd "$(dirname "$0")"
+HERE=$( (
+    cd "$(dirname "$0")"
+    echo "${PWD}"
+))
 
-. "./eclair_settings.sh"
+. "${HERE}/eclair_settings.sh"
 
-gzip -c "${ECLAIR_REPORTS_SARIF}" | base64 -w0 > reports.sarif.gz.b64
+sarif=${HERE}/reports.sarif.gz.b64
+
+gzip -c "${ECLAIR_REPORTS_SARIF}" | base64 -w0 > "${sarif}"
 
 gh api --method POST -H "Accept: application/vnd.github+json" \
-    /repos/"${GITHUB_REPOSITORY}"/code-scanning/sarifs \
-    -f commit_sha="${GITHUB_SHA}" -f ref="${GITHUB_REF}" -F sarif=@reports.sarif.gz.b64
+    "/repos/${GITHUB_REPOSITORY}/code-scanning/sarifs" \
+    -f "commit_sha=${GITHUB_SHA}" -f "ref=${GITHUB_REF}" -F "sarif=@${sarif}"
